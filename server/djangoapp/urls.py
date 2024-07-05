1ab4cd12934604761a80c9e3193242b9337bdcad
hhcsourcebank <hhcsourcebank@gmail.com>
2024-07-05 08:41:52 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,15 +10,16 @@ urlpatterns = [
     path(route='login', view=views.registration, name='login'),
 
     # path for login
-    
     path(route='login', view=views.login_user, name='login'),
 
     # path for logout
-    
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
     # path for add a review view
+
+    #path for get the car list
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
