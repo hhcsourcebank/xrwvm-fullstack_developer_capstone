ec8ce0397a7146b8fb4629bcccd61a0cb6732499
hhcsourcebank <hhcsourcebank@gmail.com>
2024-07-05 09:33:03 -0400

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -28,7 +28,7 @@ def get_request(endpoint, **kwargs):
     except:
         # If any error occurs
         print("Network exception occurred")
-        
+
 # Add code for get requests to back end
 
 def analyze_review_sentiments(text):
@@ -43,4 +43,12 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 # def post_review(data_dict):
-# Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
+
